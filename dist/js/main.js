60aa0eec9a5e6a49e9f802964972ddd723a3dd64
http404ntfnd <dmutro.tutarenko@gmail.com>
2019-03-28 20:34:28 +0200

    change services, header

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,36 +1,36 @@
 (function($) {
-    $(function() {
-     
-      $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
-     
+  $(function() {
+   
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
+   
+  });
 })(jQuery);
 
 $(function() {
 
-  $(".button").on("click", function() {
+$(".button").on("click", function() {
 
-    var $button = $(this);
-    var oldValue = $button.parent().find("input").val();
+  var $button = $(this);
+  var oldValue = $button.parent().find("input").val();
 
-    if ($button.text() == "+") {
-  	  var newVal = parseFloat(oldValue) + 1;
-  	} else {
-	   // Don't allow decrementing below zero
-      if (oldValue > 0) {
-        var newVal = parseFloat(oldValue) - 1;
-	    } else {
-        newVal = 0;
-      }
-	  }
+  if ($button.text() == "+") {
+    var newVal = parseFloat(oldValue) + 1;
+  } else {
+   // Don't allow decrementing below zero
+    if (oldValue > 0) {
+      var newVal = parseFloat(oldValue) - 1;
+    } else {
+      newVal = 0;
+    }
+  }
 
-    $button.parent().find("input").val(newVal);
+  $button.parent().find("input").val(newVal);
 
-  });
+});
 
 });
 
@@ -42,18 +42,26 @@ $(function() {
   $(".infoblock-close").on("click", function() {
     $(this).parent('.infoblock').slideToggle('fast');
   });
+
+  $(".services__boostlist-item--footer .buy").on("click", function() {
+    $('.overlay, .popup').slideToggle('fast');
+  });
+
+  $(".overlay, .popup__close").on("click", function() {
+    $('.overlay, .popup').slideToggle('fast');
+  });
 });
 
 (function($) {
-  $(function() {
-   
-    $('ul.services__herolist').on('click', 'li:not(.active)', function() {
-      $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.services-wrap').find('ul.services__boostlist').removeClass('active').eq($(this).index()).addClass('active');
-    });
-
-
-   
+$(function() {
+ 
+  $('ul.services__herolist').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.services-wrap').find('ul.services__boostlist').removeClass('active').eq($(this).index()).addClass('active');
   });
+
+
+ 
+});
 })(jQuery);
