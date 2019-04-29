3d7d20f1717dbb6657cb6846b2160854b4a218d2
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2019-04-30 02:28:14 +0300

    fix card scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,13 +7,12 @@ $(".main").onepage_scroll({
   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-  loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-  keyboard: true,                  // You can activate the keyboard controls
-  responsiveFallback: 1280,        // You can fallback to normal page scroll by defining the width of the browser in which
-                                   // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-                                   // the browser's width is less than 600, the fallback will kick in.
-  direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+  loop: true,                    
+  keyboard: true,                
+  responsiveFallback: 1280,        
 });
+
+
 
 (function($) {
   $(function() {
@@ -84,17 +83,6 @@ $(function() {
 })(jQuery);
 
 
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  var introLink = document.getElementById('intro__link'); 
-  var servicesBlock = document.getElementById('services');
-
-  function handleButtonClick() {
-    servicesBlock.scrollIntoView({block: "start", behavior: "smooth"});
-  }
-
-  introLink.addEventListener('click', handleButtonClick);
-});
 
 
 
